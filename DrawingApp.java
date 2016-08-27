e86fa81dd27f7e6e80b9c64a7a4182976528d32d
Rakesh Kumar Regar <jrakesh.iitb@gmail.com>
2016-08-28 00:36:24 +0530

    Add files via upload

---
+++ DrawingApp.java
@@ -0,0 +1,20 @@
+package com.rakesh.example.springbeans;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+@SuppressWarnings("deprecation")
+public class DrawingApp {
+
+	public static void main(String[] args) {
+//		Triangle triangle = new Triangle();
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle)context.getBean("triangle");
+		triangle.Draw();
+	}
+
+}
