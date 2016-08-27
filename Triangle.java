79896d80d3add104f304757fcc1989fd08966e6f
Rakesh Kumar Regar <jrakesh.iitb@gmail.com>
2016-08-28 01:54:37 +0530

    Bean AutoWiring

--- Triangle.java
+++ Triangle.java
@@ -1,32 +1,46 @@
 package com.rakesh.example.springbeans;
+import java.util.* ;
+
 public class Triangle {
+	
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
 	
+
 	public Point getPointA() {
 		return pointA;
 	}
+
+
 	public void setPointA(Point pointA) {
 		this.pointA = pointA;
 	}
+
+
 	public Point getPointB() {
 		return pointB;
 	}
+
+
 	public void setPointB(Point pointB) {
 		this.pointB = pointB;
 	}
+
+
 	public Point getPointC() {
 		return pointC;
 	}
+
+
 	public void setPointC(Point pointC) {
 		this.pointC = pointC;
 	}
-	
-	
+
+
 	public void Draw(){
-		System.out.println("Point A (" + getPointA().getX() + " , " + getPointA().getY()+ " ) ");
-		System.out.println("Point B (" + getPointB().getX() + " , " + getPointB().getY()+ " ) ");
-		System.out.println("Point C (" + getPointC().getX() + " , " + getPointC().getY()+ " ) ");
+		System.out.println("Point ( " + getPointA().getX() + "," + getPointA().getY() + " )");
+		System.out.println("Point ( " + getPointB().getX() + "," + getPointB().getY() + " )");
+		System.out.println("Point ( " + getPointC().getX() + "," + getPointC().getY() + " )");
 	}
 }
